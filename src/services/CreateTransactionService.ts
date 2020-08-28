149a25a504467a0d91aa0e3db146e3f85b6db7f9
Fernando Machado <netfm5@gmail.com>
2020-08-28 15:01:19 -0300

    Adicionado validação para tipo de operação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error("Invalid type of operation. Use 'income' or 'outcome'.");
+    }
+
     if (type === 'outcome') {
       const { total } = this.transactionsRepository.getBalance();
 
